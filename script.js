854506100c5a71e8964bc8d9f93dc8ccc9cec3d5
Aryan Gaikwad <112895377+Megamind0407@users.noreply.github.com>
2024-03-01 12:52:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function scrollToElement(elementSelector, instance = 0) {
 
 
 
-const link1 = document.getElementById("link1");
+const link1 = document.getElementByClassName("container");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
