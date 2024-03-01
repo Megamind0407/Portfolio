900b161a1081722310b825b34472e5d522f1f194
Aryan Gaikwad <112895377+Megamind0407@users.noreply.github.com>
2024-03-01 16:18:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,7 @@ function scrollToElement(elementSelector, instance = 0) {
 }
 
 
-
-
-
-
-
-
-const link1 = document.getElementByClassName("container");
+const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
